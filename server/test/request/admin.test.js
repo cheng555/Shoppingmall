273efcf3c2b93a2af873779ffce3bf792a9c95e6
cheng555 <59608548+cheng555@users.noreply.github.com>
2020-01-09 09:02:47 +0800

    Update admin.test.js

--- server/test/request/admin.test.js
+++ server/test/request/admin.test.js
@@ -15,7 +15,7 @@ describe('API test', function() {
       .expect(200, done)
   })
 
-  it('respond with json adn code 200', function(done) {
+  it('login api testing', function(done) {
     request(fixed_head)
       .post('/api/admin/login')
       .set('Accept', 'application/json')
