28ede1f794a151a292d57e501605960461872b10
cheng555 <59608548+cheng555@users.noreply.github.com>
2020-01-09 09:11:10 +0800

    Update admin.test.js

--- server/test/request/admin.test.js
+++ server/test/request/admin.test.js
@@ -55,7 +55,7 @@ describe('API test', function() {
       .expect(200, done)
   })
 
-  it('respond with json adn code 200', function(done) {
+  it('replied message aoi testing', function(done) {
     request(fixed_head)
       .get('/api/admin/repliedMsg')
       .set('Accept', 'application/json')
