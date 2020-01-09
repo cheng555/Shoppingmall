c1382478bc96a58dcb7d7e95ef8dd133ba0505c2
cheng555 <59608548+cheng555@users.noreply.github.com>
2020-01-09 10:16:15 +0800

    Update mall.test.js

--- server/test/request/mall.test.js
+++ server/test/request/mall.test.js
@@ -48,13 +48,7 @@ describe('API test', function() {
       .expect(200, done)
   })
 
-  // it('respond with json adn code 200', function(done) {
-  //   request(fixed_head)
-  //     .get('/api/mall/getOrderByState')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(200, done)
-  // })
+ 
 
   it('delete oeder api testing', function(done) {
     request(fixed_head)
@@ -95,13 +89,7 @@ describe('API test', function() {
       .expect('Content-Type', /json/)
       .expect(200, done)
   })
-  // it('respond with json adn code 200', function(done) {
-  //   request(fixed_head)
-  //     .get('api/mall/getGoodsComment')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(200, done)
-  // })
+ 
   it('respond with json adn code 200', function(done) {
     request(fixed_head)
       .get('/api/mall/searchGoods')
