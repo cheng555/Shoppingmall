aa1ec9d30ed89c7217667e95995a52c544266db1
cheng555 <59608548+cheng555@users.noreply.github.com>
2020-01-09 09:21:07 +0800

    Update mall.test.js

--- server/test/request/mall.test.js
+++ server/test/request/mall.test.js
@@ -40,7 +40,7 @@ describe('API test', function() {
       .expect(200, done)
   })
 
-  it('respond with json adn code 200', function(done) {
+  it('add order api testing', function(done) {
     request(fixed_head)
       .post('/api/mall/addOrder')
       .set('Accept', 'application/json')
